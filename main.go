8a383327ba7d2e7a0741765600d8cafed5205c80
Jozef Reisinger <jozef.reisinger@gmail.com>
2026-09-10 11:04:07 +0200

    refactor: getResources returns a struct

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 		log.Fatalf("loading AWS config: %v", err)
 	}
 
-	apps, apis, lambdas, buckets, err := getResources(ctx, cfg, tags)
+	resources, err := getResources(ctx, cfg, tags)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -54,58 +54,61 @@ func main() {
 		widget.Text(dashboard.Header(tags), 24, 2),
 
 		widget.Text("## Amplify Apps", 24, 1),
-		widget.Metric(apps.Requests(cfg.Region), 8, 5),
-		widget.Metric(apps.Errors4xx(cfg.Region), 8, 5),
-		widget.Metric(apps.Errors5xx(cfg.Region), 8, 5),
+		widget.Metric(resources.Apps.Requests(cfg.Region), 8, 5),
+		widget.Metric(resources.Apps.Errors4xx(cfg.Region), 8, 5),
+		widget.Metric(resources.Apps.Errors5xx(cfg.Region), 8, 5),
 
 		widget.Text("## API Gateways", 24, 1),
-		widget.Metric(apis.Requests(cfg.Region), 8, 5),
-		widget.Metric(apis.Errors4xx(cfg.Region), 8, 5),
-		widget.Metric(apis.Errors5xx(cfg.Region), 8, 5),
+		widget.Metric(resources.APIs.Requests(cfg.Region), 8, 5),
+		widget.Metric(resources.APIs.Errors4xx(cfg.Region), 8, 5),
+		widget.Metric(resources.APIs.Errors5xx(cfg.Region), 8, 5),
 
 		widget.Text("## Lambdas", 24, 1),
-		widget.Metric(lambdas.Invocations(cfg.Region).LegendRight(), 12, 5),
-		widget.Metric(lambdas.Errors(cfg.Region).LegendRight(), 12, 5),
-		// widget.Metric(lambdas.ResponseErrors(cfg.Region), 8, 5),
+		widget.Metric(resources.Lambdas.Invocations(cfg.Region).LegendRight(), 12, 5),
+		widget.Metric(resources.Lambdas.Errors(cfg.Region).LegendRight(), 12, 5),
+		// widget.Metric(resources.Lambdas.ResponseErrors(cfg.Region), 8, 5),
 
 		widget.Text("## S3 Buckets", 24, 1),
-		widget.Metric(buckets.Size(cfg.Region).ShowUnits().LegendRight(), 12, 5),
-		widget.Metric(buckets.Objects(cfg.Region).ShowUnits().LegendRight(), 12, 5),
+		widget.Metric(resources.Buckets.Size(cfg.Region).ShowUnits().LegendRight(), 12, 5),
+		widget.Metric(resources.Buckets.Objects(cfg.Region).ShowUnits().LegendRight(), 12, 5),
 	); err != nil {
 		log.Fatal(err)
 	}
 }
 
+// resources holds the AWS resources a dashboard is built from.
+type resources struct {
+	Apps    amplify.Apps
+	APIs    apigateway.RESTs
+	Lambdas lambda.Lambdas
+	Buckets s3.Buckets
+}
+
 // getResources concurrently retrieves selected AWS resources matching tags. It
 // returns the first error encountered, if any.
-func getResources(ctx context.Context, cfg aws.Config, tags resource.Tags) (amplify.Apps, apigateway.RESTs, lambda.Lambdas, s3.Buckets, error) {
-	var (
-		apps    amplify.Apps
-		apis    apigateway.RESTs
-		lambdas lambda.Lambdas
-		buckets s3.Buckets
-	)
+func getResources(ctx context.Context, cfg aws.Config, tags resource.Tags) (resources, error) {
+	var r resources
 
 	g, gctx := errgroup.WithContext(ctx)
 	g.Go(func() (err error) {
-		apps, err = amplify.Get(gctx, cfg, tags)
-		log.Printf("found %d amplify apps\n", len(apps))
+		r.Apps, err = amplify.Get(gctx, cfg, tags)
+		log.Printf("found %d amplify apps\n", len(r.Apps))
 		return err
 	})
 	g.Go(func() (err error) {
-		apis, err = apigateway.Get(gctx, cfg, tags)
-		log.Printf("found %d api gateways\n", len(apis))
+		r.APIs, err = apigateway.Get(gctx, cfg, tags)
+		log.Printf("found %d api gateways\n", len(r.APIs))
 		return err
 	})
 	g.Go(func() (err error) {
-		lambdas, err = lambda.Get(gctx, cfg, tags)
-		log.Printf("found %d lambdas\n", len(lambdas))
+		r.Lambdas, err = lambda.Get(gctx, cfg, tags)
+		log.Printf("found %d lambdas\n", len(r.Lambdas))
 		return err
 	})
 	g.Go(func() (err error) {
-		buckets, err = s3.Get(gctx, cfg, tags)
-		log.Printf("found %d s3 buckets\n", len(buckets))
+		r.Buckets, err = s3.Get(gctx, cfg, tags)
+		log.Printf("found %d s3 buckets\n", len(r.Buckets))
 		return err
 	})
-	return apps, apis, lambdas, buckets, g.Wait()
+	return r, g.Wait()
 }
